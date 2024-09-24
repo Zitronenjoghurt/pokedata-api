4f73bc2401938d4e6d0ac6b576a0139d34d229b8
Zitronenjoghurt <gersdorfdominik@gmail.com>
2024-09-24 19:50:08 +0200

    extend sprite building structure

--- commands/src/commands/pokeapi.rs
+++ commands/src/commands/pokeapi.rs
@@ -1,14 +1,15 @@
 use crate::cli::CliCommand;
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command};
 
 mod build_sprites;
 mod coverage;
 
+const CSV_DATA_PATH: &'static str = "data/v2/csv";
+const IGNORE_FILE_PREFIXES: &[&str] = &["conquest", "pokemon_species_prose", "super_contest", "type_game_indices"];
 const POKEAPI_OWNER: &'static str = "PokeAPI";
 const POKEAPI_REPO: &'static str = "pokeapi";
 const POKEAPI_SPRITES_REPO_HTTPS: &'static str = "https://github.com/PokeAPI/sprites.git";
-const CSV_DATA_PATH: &'static str = "data/v2/csv";
-const IGNORE_FILE_PREFIXES: &[&str] = &["conquest", "pokemon_species_prose", "super_contest", "type_game_indices"];
+const POKEMON_SPRITES_INDEX_CONFIG_PATH: &'static str = "./configuration/pokeapi_pokemon_sprites_index.json";
 
 pub struct PokeApiCommand;
 
@@ -26,14 +27,23 @@ impl CliCommand for PokeApiCommand {
             )
             .subcommand(
                 Command::new("build-sprites")
-                    .about("Will download and build a sprite index with tokenized file names")
+                    .about("Will download and build a sprite index")
+                    .arg(
+                        Arg::new("sh")
+                            .long("self-host")
+                            .help("Enable self-hosting mode. This will tokenize the file names and copy them to a separate section of the data directory (see documentation).")
+                            .action(clap::ArgAction::SetTrue)
+                    )
             )
     }
 
     fn run(&self, args: &ArgMatches) -> Result<(), String> {
         match args.subcommand() {
             Some(("coverage", _)) => { coverage::coverage() }
-            Some(("build-sprites", _)) => { build_sprites::build_sprites() }
+            Some(("build-sprites", sub_matches)) => {
+                let self_host = sub_matches.get_flag("sh");
+                build_sprites::build_sprites(self_host)
+            }
             _ => {
                 Err("Try using ./cli pokeapi --help'".to_string())
             }
--- commands/src/commands/pokeapi/build_sprites.rs
+++ commands/src/commands/pokeapi/build_sprites.rs
@@ -1,23 +1,48 @@
-use crate::commands::pokeapi::POKEAPI_SPRITES_REPO_HTTPS;
+use crate::commands::pokeapi::{POKEAPI_SPRITES_REPO_HTTPS, POKEMON_SPRITES_INDEX_CONFIG_PATH};
 use git2::Repository;
 use pokedata_api_utils::filesystem::{create_directory, get_data_path};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
 use std::path::PathBuf;
 
-pub fn build_sprites() -> Result<(), String> {
+/// HashMap<SpriteGroup, HashMap<SpriteType, Path>>
+#[derive(Debug, Serialize, Deserialize)]
+struct SpritePaths(HashMap<String, HashMap<String, String>>);
+
+/// HashMap<PokemonId, HashMap<SpriteType, FilePath>>
+/// PokemonId can include an optional form identifier appended with a hyphen, like: 493-bug for Arceus bug form
+///
+/// When self-host is enabled, the filepath will be the tokenized file names,
+/// else they will be the path relative to the public PokeAPI sprites repository at:
+/// https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/
+/// for example:
+/// "pokemon/1.png" points to https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/1.png
+#[derive(Debug, Serialize, Deserialize)]
+struct SpriteIndex(HashMap<String, HashMap<String, String>>);
+
+pub fn build_sprites(self_host: bool) -> Result<(), String> {
     let data_path = get_data_path().unwrap();
-    let sprites_path = data_path.join("sprites");
-    if !sprites_path.exists() {
-        clone_repository(&sprites_path)?;
+    let repo_path = data_path.join("sprites-repo");
+    let self_host_path = data_path.join("sprites-self-host");
+
+    if !repo_path.exists() {
+        clone_repository(&repo_path)?;
     } else {
         println!("Found cloned sprites repository");
     }
 
-    // ToDo: analyze files
+    if self_host {
+        create_directory(&self_host_path);
+    }
+
+    build_pokemon_sprites(&repo_path, &self_host_path, self_host);
 
     Ok(())
 }
 
-pub fn clone_repository(sprites_path: &PathBuf) -> Result<(), String> {
+fn clone_repository(sprites_path: &PathBuf) -> Result<(), String> {
     create_directory(&sprites_path);
     println!("Cloning sprites repository... (this might take a bit)");
     match Repository::clone(POKEAPI_SPRITES_REPO_HTTPS, &sprites_path) {
@@ -26,4 +51,22 @@ pub fn clone_repository(sprites_path: &PathBuf) -> Result<(), String> {
     };
 
     Ok(())
+}
+
+fn build_sprites_from_paths(
+    repo_base_path: &PathBuf,
+    sub_folder_name: &str,
+    sprite_paths: SpritePaths,
+    self_host_path: &PathBuf,
+    self_host: bool,
+)
+{
+    let sprites_directory = repo_base_path.join("sprites").join(sub_folder_name);
+}
+
+fn build_pokemon_sprites(repo_path: &PathBuf, self_host_path: &PathBuf, self_host: bool) {
+    let file = File::open(POKEMON_SPRITES_INDEX_CONFIG_PATH).unwrap();
+    let reader = BufReader::new(file);
+    let pokemon_sprite_paths: SpritePaths = serde_json::from_reader(reader).unwrap();
+    build_sprites_from_paths(repo_path, "pokemon", pokemon_sprite_paths, self_host_path, self_host);
 }
